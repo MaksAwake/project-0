cf3f04435480c7e2986ec78eb0b760f17de4f4ec
maksawake <awakeawake666@gmail.com>
2023-01-09 13:03:29 +0200

    second

--- js/script.js
+++ js/script.js
@@ -8,12 +8,23 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-const a = prompt("Один из последних просмотренных фильмов?","");
-const b = prompt("На сколько его оцените?","");
-const c = prompt("Один из последних просмотренных фильмов?","");
-const d = prompt("На сколько его оцените?","");
+for(let i = 0; i< 2; i++){
+     const a = prompt("Один из последних просмотренных фильмов?",""),
+          b = prompt("На сколько его оцените?","");
+    if(a !=null && b != null && a !='' && b!='' && a.length <=50){
+        personalMovieDB.movies [a] = b;
+    } else{ i--;
+    }
+}
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+if(personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.count>=30){
+    console.log("Вы киноман");
+} else{
+    console.log("Произошла ошибка")
+}
 
 console.log(personalMovieDB);
